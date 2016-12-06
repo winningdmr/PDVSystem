0055f2c22d01817098f3c4c8eff497548ed72573
winingdmr <pablo.tecnologia@lafisum.com>
2016-12-06 17:20:30 -0500

    revision laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Pablo
                 </div>
 
                 <div class="links">
